04c0e781c255547754144ebabf86f705c6d26d91
Keith Bechtol <kbechtol@lsst.org>
2018-04-07 10:50:53 -0700

    Performance increase

--- bokeh_m2_example.py
+++ bokeh_m2_example.py
@@ -16,7 +16,7 @@ data = reader[1].data
 reader.close()
 
 #data = data[0:1000]
-data = data[data['MAG_AUTO_G_DERED'] < 24.]
+data = data[data['MAG_AUTO_G_DERED'] < 26.]
 
 print len(data)
 
@@ -50,7 +50,7 @@ TOOLS_RIGHT = [hover_right, TOOLS]
 #TOOLS_RIGHT = ["box_zoom,box_select,lasso_select,hover,reset,help"]
 
 # create a new plot and add a renderer
-left = figure(tools=TOOLS_LEFT, plot_width=500, plot_height=500, 
+left = figure(tools=TOOLS_LEFT, plot_width=500, plot_height=500, output_backend="webgl",
               title='Spatial: Centered on (RA, Dec) = (%.2f, %.2f)'%(ra_target, dec_target))
 left.circle('x0', 'y0', hover_color="firebrick", source=source)
 left.x_range = Range1d(0.3, -0.3)
@@ -59,10 +59,11 @@ left.xaxis.axis_label = 'Delta RA'
 left.yaxis.axis_label = 'Delta DEC'
 
 # create another new plot and add a renderer
-right = figure(tools=TOOLS_RIGHT, plot_width=500, plot_height=500, title='CMD')
+right = figure(tools=TOOLS_RIGHT, plot_width=500, plot_height=500, output_backend="webgl",
+               title='CMD')
 right.circle('x1', 'y1', hover_color="firebrick", source=source)
 right.x_range = Range1d(-0.5, 2.5)
-right.y_range = Range1d(24., 16.)
+right.y_range = Range1d(26., 16.)
 right.xaxis.axis_label = 'g - r'
 right.yaxis.axis_label = 'g'
 
